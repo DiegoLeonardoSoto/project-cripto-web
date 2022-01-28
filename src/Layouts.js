133307a6219abba755c4cfa7447dc9d96c8c5a18
unknown <leonardo.soto123@hotmail.com>
2022-01-28 01:42:12 -0300

    se inicio el proyecto

---
+++ src/Layouts.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const InnerLayout = styled.div`
+    padding: 4rem 15rem;
+`;
+
+const SectionLayout = styled.section`
+    padding: 4rem 0rem;
+`;
+
+export {InnerLayout, SectionLayout}
